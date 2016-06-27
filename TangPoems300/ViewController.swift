9a7250a21decb96f70a57dc43a031ca8a77dee27
EricYang <ericyang@ericyangdemacbook-pro.local>
2016-06-27 14:52:41 +0800

    Initial Commit

---
+++ TangPoems300/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TangPoems300
+//
+//  Created by EricYang on 2016/6/27.
+//  Copyright © 2016年 EricYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
